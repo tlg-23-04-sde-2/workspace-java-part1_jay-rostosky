5e8d6451eb409df871bdf7db1436ac1f45f01223
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-05-11 14:18:12 -0700

    Lab11.1 complete

---
+++ Lab11.1/src/com/hr/corp/Corporation.java
@@ -0,0 +1,48 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+
+package com.hr.corp;
+
+import gov.irs.TaxPayer;
+
+/**
+ * Corporate entity - also pays taxes.
+ */
+public class Corporation implements TaxPayer {
+    public static final double STANDARD_DEDUCTION = 1_000_000.0;
+
+    private String name;
+
+    public Corporation(String name) {
+        super();
+        setName(name);
+    }
+
+    @Override  // interface TaxPayer
+    public void payTaxes() {
+        System.out.println(getName() + " paid no taxes - we lobbied hard and it worked");
+    }
+
+    @Override  // interface TaxPayer
+    public void fileReturn() {
+        System.out.println("Return not filed - we sent our lawyers instead");
+    }
+
+    @Override  // interface TaxPayer
+    public double getStandardDeduction() {
+        return STANDARD_DEDUCTION;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
